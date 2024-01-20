a4aaf7c0860a6f82df4011992eabba1296fbe1c8
PulkitSinha <pulkitsinhaor@gmail.com>
2024-01-20 18:06:44 +0530

    navbar and sidebar navigation

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -18,7 +18,7 @@ const NavContent = () => {
   const pathname = usePathname();
 
   return (
-    <section className="flex h-full flex-col gap-6 pt-16">
+    <section className="flex h-full flex-col gap-3 pt-[7%]">
       {sidebarLinks.map((item) => {
         const isActive =
           (pathname.includes(item.route) && item.route.length > 1) ||
