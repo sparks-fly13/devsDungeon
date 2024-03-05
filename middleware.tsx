03cef90dcf015d397ef8de1f682dc8170f973d26
PulkitSinha <pulkitsinhaor@gmail.com>
2024-03-05 16:33:07 +0530

    add api webhook

--- middleware.tsx
+++ middleware.tsx
@@ -4,7 +4,17 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks(.*)"],
+  publicRoutes: [
+    "/",
+    "/api/webhooks(.*)",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhooks(.*)", "/api/chatgpt"],
 });
 
 export const config = {
